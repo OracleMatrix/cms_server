032144e2b16d9ee47e103d6593942c96ef998a3a
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-10-03 20:09:07 +0330

    feat(routes): add comment routes
    Added comment routes to the API, allowing for CRUD operations on comments.

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/api/v1/', require('./routes/auth.routes'));
 app.use('/api/v1/', require('./routes/user.routes'));
 app.use('/api/v1/', require('./routes/post.routes'));
+app.use('/api/v1/', require('./routes/comment.routes'));
 
 // RUN SERVER
 db.sequelize.sync().then(() => {
