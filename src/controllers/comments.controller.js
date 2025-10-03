0481226a8326b50e029b1fe671c72ef95e39bdd2
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-10-03 20:08:52 +0330

    feat(comments.controller): add comments controller with CRUD operations
    Added a new comments controller with methods for getting a comment by ID, getting comments by user ID, getting all comments, creating a new comment, and deleting a comment.

---
+++ src/controllers/comments.controller.js
@@ -0,0 +1,52 @@
+const commentsService = require('../services/comments.service');
+
+class CommentsController {
+    async getCommentById(req, res) {
+        try {
+            const data = await commentsService.getCommentById(req.params.commentId);
+            return res.status(200).send(data);
+        }  catch (error) {
+            return res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+
+    async getCommentByUserId(req, res) {
+        try {
+            const {page, limit} = req.query;
+            const data = commentsService.getCommentsByUserId(req.params.userId, parseInt(page) || 1, parseInt(limit) || 10);
+            return res.status(200).send(data);
+        }  catch (error) {
+            return res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+
+    async getAllComments(req, res) {
+        try {
+            const { page, limit } = req.query;
+            const data = await commentsService.getAllComments(parseInt(page) || 1, parseInt(limit) || 10);
+            return res.status(200).send(data);
+        }   catch (error) {
+            return res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+
+    async createComment(req, res) {
+        try {
+            const data = await commentsService.createComment(req.params.userId, req.params.postId ,req.body);
+            return res.status(201).json(data);
+        }   catch (error)  {
+            return res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+
+    async deleteComment(req, res) {
+        try {
+            await commentsService.deleteComment(req.params.commentId);
+            return res.status(200).send({message: 'Comment has been deleted'});
+        } catch(error) {
+            return res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+}
+
+module.exports = new CommentsController();
