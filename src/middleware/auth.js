784c1517d60b417d9886b84c74067d93d7313182
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-10-27 19:18:58 +0330

    added authenticate middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const db = require('../models');
+const UsersModel = db.users;
+
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await UsersModel.findByPk(decoded.id);
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+module.exports = authMiddleware;
