290c2dfa6a312a2c14029b1756cd89f240123a49
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-10-29 11:59:23 +0330

    `Update Joi validation schema in AuthService to include 'user' role`

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -9,17 +9,17 @@ class AuthService {
         const bodySchema = Joi.object({
             email: Joi.string().email().required(),
             password: Joi.string().min(8).max(255).required(),
-            role: Joi.string().valid('author', 'editor', 'admin').required(),
+            role: Joi.string().valid('author', 'editor', 'admin', 'user').required(),
         });
-        const {error} = bodySchema.validate(data);
+        const { error } = bodySchema.validate(data);
         if (error) {
             const errorMessage = new Error(error.details[0].message);
             errorMessage.status = 400;
             throw errorMessage;
         }
-        const {email, password, role} = data;
+        const { email, password, role } = data;
 
-        const userExists = await UsersModel.findOne({where: {email: email, role: role}});
+        const userExists = await UsersModel.findOne({ where: { email: email, role: role } });
         if (userExists) {
             const userExistsError = new Error("User already exists");
             userExistsError.status = 400;
@@ -32,9 +32,9 @@ class AuthService {
             role: role,
         });
 
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
-        return {user: _.pick(user, ['id', 'userName', 'email', 'role', 'createdAt', 'updatedAt']), token: token};
+        return { user: _.pick(user, ['id', 'userName', 'email', 'role', 'createdAt', 'updatedAt']), token: token };
     }
 
     async loginUser(data) {
@@ -42,14 +42,14 @@ class AuthService {
             email: Joi.string().email().required(),
             password: Joi.string().min(8).max(255).required(),
         });
-        const {error} = bodySchema.validate(data);
+        const { error } = bodySchema.validate(data);
         if (error) {
             const errorMessage = new Error(error.details[0].message);
             errorMessage.status = 400;
             throw errorMessage;
         }
-        const {email, password} = data;
-        const user = await UsersModel.findOne({where: {email: email}});
+        const { email, password } = data;
+        const user = await UsersModel.findOne({ where: { email: email } });
         if (!user) {
             const userExistsError = new Error("User not found");
             userExistsError.status = 404;
@@ -61,9 +61,9 @@ class AuthService {
             errorMessage.status = 400;
             throw errorMessage;
         }
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
-        return {user: _.pick(user, ['id', 'userName', 'email', 'role', 'createdAt', 'updatedAt']), token: token};
+        return { user: _.pick(user, ['id', 'userName', 'email', 'role', 'createdAt', 'updatedAt']), token: token };
     }
 }
 
