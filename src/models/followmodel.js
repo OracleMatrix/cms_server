b0389da7b1d6005730190a6c2ffe1a1a3cae2750
oraclematrix <ehsanmohamadipoor@gmail.com>
2025-11-08 22:50:27 +0330

    `Update follow model to include foreign key constraints`

--- src/models/followmodel.js
+++ src/models/followmodel.js
@@ -31,7 +31,16 @@ module.exports = (sequelize, DataTypes) => {
         onUpdate: 'CASCADE',
       }
     },
-    followingId: DataTypes.INTEGER
+    followingId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }
+    },
   }, {
     sequelize,
     modelName: 'follow',
